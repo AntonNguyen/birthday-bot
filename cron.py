e03e3c6ef0f85e05be27db31599595e0fb252235
Anton Nguyen <anton.n@ecobee.com>
2019-04-03 14:15:47 -0400

    Calvin forgot to send this file

---
+++ cron.py
@@ -0,0 +1,22 @@
+from common import get_email_content
+from email.message import Message
+from smtplib import SMTP
+import datetime
+import os
+
+if __name__ == '__main__':
+    content = get_email_content()
+
+    if len(content) > 0:
+        msg = Message()
+        msg['Subject'] = 'Birthdays and Freshiversaries for ' + datetime.date.today().strftime('%B %-d, %Y')
+        msg['From'] = 'TimBot <timbot@freshbooks.com>'
+        msg['To'] = 'wc@freshbooks.com'
+        msg.add_header('Content-Type','text/html')
+        msg.set_payload(content)
+
+        smtp = SMTP('smtp.office365.com', 587)
+        smtp.starttls()
+        smtp.login('timbot@freshbooks.com', os.environ['TIMBOT_EMAIL_PASSWORD'])
+        smtp.sendmail('timbot@freshbooks.com', [msg['To']], msg.as_string())
+        smtp.quit()
